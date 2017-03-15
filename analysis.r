21b4f61ed02b18288011d7adf2eeda792f8daaa9
Harini Veeraraghavan <veerarah@mskcc.org>
2017-03-15 15:35:42 -0400

    Updated with Cox regression analysis

--- analysis.r
+++ analysis.r
@@ -188,10 +188,23 @@ title('(d) TCIA: Texture-Clustering PFS')
 dataMSK["dSites"] <- as.numeric(dataMSK$Sites > median(dataMSK$Sites))
 dataTCGA["dSites"] <- as.numeric(dataTCGA$Sites > median(dataTCGA$Sites))
 
+#mod.mskccSitesVolumeClustHetPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
+#                                            Sites+Volume+ClusterIDISTH+ClusterIDTextures,
+#                                         data=dataMSK)
+### ISTH measures
 mod.mskccSitesVolumeClustHetPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
-                                            Sites+Volume+ClusterIDISTH+ClusterIDTextures,
+                                           Sites+Volume+ClusterIDISTH,
                                          data=dataMSK)
+
 summary(mod.mskccSitesVolumeClustHetPFS)
+
+## Haralick measures
+mod.mskccSitesVolumeClustHetPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
+                                           Sites+Volume+ClusterIDTextures,
+                                         data=dataMSK)
+
+summary(mod.mskccSitesVolumeClustHetPFS)
+
 
 ## using discrete sites >7 versus <= 7
 mod.mskccSitesVolumeClustHetPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
@@ -201,9 +214,18 @@ summary(mod.mskccSitesVolumeClustHetPFS)
 
 
 ###OS
+## ISTH
 mod.mskccSitesVolumeClustHetOS <- coxph(Surv(dataMSK$OS, dataMSK$status)~
-                                          Sites+Volume+ClusterIDISTH+ClusterIDTextures,
+                                          Sites+Volume+ClusterIDISTH,
                                         data=dataMSK)
+
+summary(mod.mskccSitesVolumeClustHetOS)
+
+## Haralick
+mod.mskccSitesVolumeClustHetOS <- coxph(Surv(dataMSK$OS, dataMSK$status)~
+                                          Sites+Volume+ClusterIDTextures,
+                                        data=dataMSK)
+
 summary(mod.mskccSitesVolumeClustHetOS)
 
 ## using discrete sites >7 versus <= 7
@@ -216,8 +238,15 @@ summary(mod.mskccSitesVolumeClustHetOS)
 
 #### TCIA
 ##PFS
+#ISTH
 mod.tcgaSitesVolumeClustHetPFS <- coxph(Surv(dataTCGA$PFS, dataTCGA$Recurrence)~
-                                          Sites+Volume+ClusterIDISTH+ClusterIDTextures,data=dataTCGA)
+                                          Sites+Volume+ClusterIDISTH,data=dataTCGA)
+
+summary(mod.tcgaSitesVolumeClustHetPFS)
+
+# Haralick
+mod.tcgaSitesVolumeClustHetPFS <- coxph(Surv(dataTCGA$PFS, dataTCGA$Recurrence)~
+                                          Sites+Volume+ClusterIDTextures,data=dataTCGA)
 summary(mod.tcgaSitesVolumeClustHetPFS)
 
 ## using discrete sites >4 versus <=4
@@ -227,8 +256,16 @@ summary(mod.tcgaSitesVolumeClustHetPFS)
 
 
 ###OS
+##ISTH
 mod.tcgaSitesVolumeClustHetOS <- coxph(Surv(dataTCGA$OS, dataTCGA$status)~
-                                         Sites+Volume+ClusterIDISTH+ClusterIDTextures,data=dataTCGA)
+                                         Sites+Volume+ClusterIDISTH,data=dataTCGA)
+
+summary(mod.tcgaSitesVolumeClustHetOS)
+
+#Haralick
+mod.tcgaSitesVolumeClustHetOS <- coxph(Surv(dataTCGA$OS, dataTCGA$status)~
+                                         Sites+Volume+ClusterIDTextures,data=dataTCGA)
+
 summary(mod.tcgaSitesVolumeClustHetOS)
 
 ## using discrete sites > 4 versus <= 4
@@ -264,24 +301,76 @@ summary(mod.tcgaISTHVolumeSitesOS)
 ##MSKCC 
 #PFS
 mod.mskccISTHPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
-                                       cSE+cSCV+cSCP, data=dataMSK)
+                                       cSE+cSCP, data=dataMSK)
 summary(mod.mskccISTHPFS)
 
 #OS
 mod.mskccISTHOS <- coxph(Surv(dataMSK$OS, dataMSK$status)~
-                                      cSE+cSCV+cSCP, data=dataMSK)
+                                      cSE+cSCP, data=dataMSK)
 summary(mod.mskccISTHOS)
 
 ## TCIA
 #PFS
 mod.tcgaISTHPFS <- coxph(Surv(dataTCGA$PFS, dataTCGA$Recurrence)~
-                                      cSE+cSCV+cSCP, data=dataTCGA)
+                                      cSE+cSCP, data=dataTCGA)
 summary(mod.tcgaISTHPFS)
 
 #OS
 mod.tcgaISTHOS <- coxph(Surv(dataTCGA$OS, dataTCGA$status)~
-                                     cSE+cSCV+cSCP, data=dataTCGA)
+                                     cSE+cSCP, data=dataTCGA)
 summary(mod.tcgaISTHOS)
+
+
+## Cox using only the Haralick textures and Volume + Sites
+##MSKCC 
+#PFS
+mod.mskccHaralickPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
+                                Volume+Sites+Energy+Entropy+Homogeneity+Contrast, data=dataMSK)
+summary(mod.mskccHaralickPFS)
+
+#OS
+mod.mskccHaralickOS <- coxph(Surv(dataMSK$OS, dataMSK$status)~
+                               Volume+Sites+Energy+Entropy+Homogeneity+Contrast, data=dataMSK)
+summary(mod.mskccHaralickOS)
+
+## TCIA
+#PFS
+mod.tcgaHaralickPFS <- coxph(Surv(dataTCGA$PFS, dataTCGA$Recurrence)~
+                               Volume+Sites+Energy+Entropy+Homogeneity+Contrast, data=dataTCGA)
+summary(mod.tcgaHaralickPFS)
+
+#OS
+mod.tcgaHaralickOS <- coxph(Surv(dataTCGA$OS, dataTCGA$status)~
+                              Volume+Sites+Energy+Entropy+Homogeneity+Contrast, data=dataTCGA)
+summary(mod.tcgaHaralickOS)
+
+
+
+
+## Cox using only the Haralick textures
+##MSKCC 
+#PFS
+mod.mskccHaralickPFS <- coxph(Surv(dataMSK$PFS, dataMSK$Recurrence)~
+                            Energy+Entropy+Homogeneity+Contrast, data=dataMSK)
+summary(mod.mskccHaralickPFS)
+
+#OS
+mod.mskccHaralickOS <- coxph(Surv(dataMSK$OS, dataMSK$status)~
+                           Energy+Entropy+Homogeneity+Contrast, data=dataMSK)
+summary(mod.mskccHaralickOS)
+
+## TCIA
+#PFS
+mod.tcgaHaralickPFS <- coxph(Surv(dataTCGA$PFS, dataTCGA$Recurrence)~
+                           Energy+Entropy+Homogeneity+Contrast, data=dataTCGA)
+summary(mod.tcgaHaralickPFS)
+
+#OS
+mod.tcgaHaralickOS <- coxph(Surv(dataTCGA$OS, dataTCGA$status)~
+                          Energy+Entropy+Homogeneity+Contrast, data=dataTCGA)
+summary(mod.tcgaHaralickOS)
+
+
 
 ########## Survival curves using Sites
 par(mfrow=c(2,2), mar=c(4,5,3,1))
@@ -298,13 +387,13 @@ mfitHi <- survfit(Surv(monthsHi, dataMSK$Recurrence[sHi])~1)
 plot(mfitLo, conf.int=FALSE, mark.time = TRUE, mark= 3, col='royalblue3', xaxs="S", xlab='Time(months)', ylab="Proportion PFS", lwd=3, cex.axis=1.25, cex.lab=1.5, font=2.0)
 lines(mfitHi, conf.int=FALSE, mark.time = TRUE, mark = 3, col='orangered', xaxs="S", xlab='Time(months)', ylab="Proportion PFS", lwd=3, lty=1)
 
-legend(x="topright", .6, c("Num Sites <= 7", "Num Sites > 7"), 
+legend(x="topright", inset=0.1, c("Num Sites <= 7", "Num Sites > 7"), 
        col=c('royalblue3', 'orangered'), 
        cex=1.5, y.intersp=1.0, lty=1:2, lwd=2, bty="n")
 title('(a) MSKCC: Number of Tumor Sites vs. PFS')
 y <-c("p=0.036")
 lab <-toString(y);
-text(10, 0.15, label=lab)
+text(10, 0.15, label=lab, font=2)
 
 
 survdiff(Surv(dataMSK$OS, dataMSK$status)~sediff)
@@ -317,13 +406,13 @@ mfitHi <- survfit(Surv(monthsHi, dataMSK$status[sHi])~1)
 plot(mfitLo, conf.int=FALSE, mark.time = TRUE, mark= 3, col='royalblue3', xaxs="S", xlab='Time(months)', ylab="Proportion OS", lwd=3, cex.axis=1.25, cex.lab=1.5, font=2.0)
 lines(mfitHi, conf.int=FALSE, mark.time = TRUE, mark = 3, col='orangered', xaxs="S", xlab='Time(months)', ylab="Proportion OS", lwd=3, lty=1)
 
-legend(x="topright", .6, c("Num Sites <= 7", "Num Sites > 7"), 
+legend(x="topright", inset=0.1, c("Num Sites <= 7", "Num Sites > 7"), 
        col=c('royalblue3', 'orangered'), 
        cex=1.5, y.intersp=1.0, lty=1:2, lwd=2, bty="n")
 title('(b) MSKCC: Number of Tumor Sites vs. OS')
 y <-c("p=0.031")
 lab <-toString(y);
-text(10, 0.15, label=lab)
+text(10, 0.15, label=lab, font=2)
 
 
 
@@ -339,16 +428,19 @@ mfitHi <- survfit(Surv(monthsHi, dataTCGA$Recurrence[sHi])~1)
 plot(mfitLo, conf.int=FALSE, mark.time = TRUE, mark= 3, col='royalblue3', xaxs="S", xlab='Time(months)', ylab="Proportion PFS", lwd=3, cex.axis=1.25, cex.lab=1.5, font=2.0)
 lines(mfitHi, conf.int=FALSE, mark.time = TRUE, mark = 3, col='orangered', xaxs="S", xlab='Time(months)', ylab="Proportion PFS", lwd=3, lty=1)
 
-legend(x="topright", .6, c("Num Sites <= 4", "Num Sites > 4"), 
+legend(x="topright", inset=0.1, c("Num Sites <= 4", "Num Sites > 4"), 
        col=c('royalblue3', 'orangered'), 
        cex=1.5, y.intersp=1.0, lty=1:2, lwd=2, bty="n")
 title('(c) TCIA: Number of Tumor Sites vs. PFS')
 y <-c("p=0.039")
 lab <-toString(y);
-text(10, 0.15, label=lab)
+text(10, 0.15, label=lab, font=2)
 
 survdiff(Surv(dataTCGA$OS, dataTCGA$status)~sediff)
 
+
+sLo <- dataTCGA$Sites <= median(dataTCGA$Sites)
+sHi <- dataTCGA$Sites > median(dataTCGA$Sites)
 montshLo <- dataTCGA$OS[sLo]
 monthsHi <- dataTCGA$OS[sHi]
 mfitLo <- survfit(Surv(monthsLo, dataTCGA$status[sLo])~1)
@@ -356,13 +448,13 @@ mfitHi <- survfit(Surv(monthsHi, dataTCGA$status[sHi])~1)
 plot(mfitLo, conf.int=FALSE, mark.time = TRUE, mark= 3, col='royalblue3', xaxs="S", xlab='Time(months)', ylab="Proportion OS", lwd=3, cex.axis=1.25, cex.lab=1.5, font=2.0)
 lines(mfitHi, conf.int=FALSE, mark.time = TRUE, mark = 3, col='orangered', xaxs="S", xlab='Time(months)', ylab="Proportion OS", lwd=3, lty=1)
 
-legend(x="topright", .6, c("Num Sites <= 4", "Num Sites > 4"), 
+legend(x="topright", inset=0.1, c("Num Sites <= 4", "Num Sites > 4"), 
        col=c('royalblue3', 'orangered'), 
        cex=1.5, y.intersp=1.0, lty=1:2, lwd=2, bty="n")
 title('(d) TCIA: Number of Tumor Sites vs. OS')
 y <-c("p=0.66")
 lab <-toString(y);
-text(10, 0.15, label=lab)
+text(10, 0.15, label=lab, font=2)
 
 ####################################################################
 ########## Clinical measures significance tests
